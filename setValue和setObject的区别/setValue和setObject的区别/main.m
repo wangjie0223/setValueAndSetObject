6c01b8578a7cc37e99213c71796eb7340358a4a7
wangjieGithub <wangjie7629@163.com>
2018-07-12 20:50:41 +0800

    no message

--- setValue和setObject的区别/setValue和setObject的区别/main.m
+++ setValue和setObject的区别/setValue和setObject的区别/main.m
@@ -18,37 +18,58 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
     
-        //setObject和setvalue的区别
         NSMutableDictionary *dic = [NSMutableDictionary dictionary];
-        NSMutableArray *array = [NSMutableArray array];
-        [array insertObject:@"1" atIndex:0];
-        [array insertObject:@"2" atIndex:1];
+
+        //**************** dic ***************
+        //nil不是对象
+        //[NSNull null] 是对象 空对象
         
-        [dic setObject:[NSNull null] forKey:@"haha"];
-        [dic setObject:[NSNull null] forKey:array];
-        [dic setObject:[NSNull null] forKey:[NSNull null]];
-        [dic setObject:@1 forKey:@""];
-        [dic setObject:@"" forKey:@1];
-//        [dic setObject:nil forKey:@""];
-//        [dic setObject:@"" forKey:nil];
-        NSLog(@"dic:%@",dic);
-        //setobject中的key和value可以为除了nil外的任何对象
-        NSLog(@"*****************");
+        //值可以为空对象
+        //[dic setObject:[NSNull null] forKey:@"haha"];
+        //值不可以为nil
+        //[dic setObject:nil forKey:@""];
         
-        [dic removeAllObjects];
-        NSLog(@"dic:%@",dic);
+        //键可以为空对象
+        //[dic setObject:[NSNull null] forKey:[NSNull null]];
+        //键不可以为nil
+        //[dic setObject:@"" forKey:nil];
         
-        [dic setValue:[NSNull null] forKey:@"haha"];
-//        [dic setValue:[NSNull null] forKey:array];
-//        [dic setValue:[NSNull null] forKey:[NSNull null]];
-        [dic setValue:@1 forKey:@""];
-        //[dic setValue:@"" forKey:@1];
-        [dic setValue:nil forKey:@""];
-        //[dic setValue:@"" forKey:nil];
-        [dic setValue:@"" forKey:@"hehe"];
-        [dic setValue:array forKey:@""];
-        NSLog(@"dic2:%@",dic);
-        //setValue中的KEY只能为字符串 value可以为nil也可以为空对象[NSNull null]
+        //@1 是对象
+        //[dic setObject:@1 forKey:@""];
+        //[dic setObject:@"" forKey:@1];
+        //NSLog(@"dic:%@",dic);
+
+       
+        
+        
+        
+        
+                NSMutableArray *array = [NSMutableArray array];
+        //        [array insertObject:@"1" atIndex:0];
+        //        [array insertObject:@"2" atIndex:1];
+
+//        [dic setObject:[NSNull null] forKey:array];
+        
+        
+
+        
+//        //setobject中的key和value可以为除了nil外的任何对象
+//        NSLog(@"*****************");
+//
+//        [dic removeAllObjects];
+//        NSLog(@"dic:%@",dic);
+//
+//        [dic setValue:[NSNull null] forKey:@"haha"];
+////        [dic setValue:[NSNull null] forKey:array];
+////        [dic setValue:[NSNull null] forKey:[NSNull null]];
+//        [dic setValue:@1 forKey:@""];
+//        //[dic setValue:@"" forKey:@1];
+//        [dic setValue:nil forKey:@""];
+//        //[dic setValue:@"" forKey:nil];
+//        [dic setValue:@"" forKey:@"hehe"];
+//        [dic setValue:array forKey:@""];
+//        NSLog(@"dic2:%@",dic);
+//        //setValue中的KEY只能为字符串 value可以为nil也可以为空对象[NSNull null]
         
        
 
